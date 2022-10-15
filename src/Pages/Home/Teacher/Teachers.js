8dd804b50f2f5f2a7a277d3d0284ae1708af8914
capmahin <mahinulhuque1996@gmail.com>
2022-10-16 03:06:14 +0600

    set card

--- src/Pages/Home/Teacher/Teachers.js
+++ src/Pages/Home/Teacher/Teachers.js
@@ -1,25 +1,72 @@
 import React from "react";
 
 const Teachers = ({ teachers }) => {
-  const { name, img, post, exprieance, fees } = teachers;
+  const { name, img, post, experience, fees } = teachers;
   return (
-    <div className="card card-side  bg-base-100 divide-y divide-dashed">
-      <figure>
-        <div className="avatar p-2">
-          <div className="w-24 h-fit rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-            <img src="https://placeimg.com/192/192/people" />
+    <div className="card card-side   bg-base-100 divide-y divide-dashed ">
+      <div className="flex">
+        {" "}
+        <figure>
+          <div className="avatar p-2">
+            <div className="w-24 h-fit rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+              <img src={img} alt="" />
+            </div>
+          </div>
+        </figure>
+        <div className="card-body p-2">
+          <h2 className=" text-md font-bold">
+            {name}{" "}
+            <span className="text-sm font-semibold text-purple-500">
+              {post}
+            </span>
+          </h2>
+
+          <p>{experience}</p>
+          <p className="text-purple-500"> Hourly fees: &{fees}</p>
+          <div className="flex">
+            <div className="rating rating-sm">
+              <input
+                type="radio"
+                name="rating-6"
+                className="mask mask-star-2 bg-purple-500"
+              />
+              <input
+                type="radio"
+                name="rating-6"
+                className="mask mask-star-2 bg-purple-500"
+                checked
+              />
+              <input
+                type="radio"
+                name="rating-6"
+                className="mask mask-star-2 bg-purple-500"
+              />
+              <input
+                type="radio"
+                name="rating-6"
+                className="mask mask-star-2 bg-purple-500"
+              />
+              <input
+                type="radio"
+                name="rating-6"
+                className="mask mask-star-2 bg-purple-500"
+              />
+            </div>
+            <div>
+              <h1 className="">4.5</h1>
+            </div>
           </div>
         </div>
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">
-          Shoes!
-          <div className="badge badge-secondary">NEW</div>
-        </h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div className="card-actions justify-end align-end">
-          <div className="badge badge-outline">Fashion</div>
-          <div className="badge badge-outline">Products</div>
+      </div>
+      <div className="card-actions justify-end align-middle  ">
+        <div className="collapse">
+          <input type="checkbox" />
+          <button className=" collapse-title badge badge-primary badge-outline">
+            Read More
+          </button>
+          <div className="collapse-content">
+            <p>hello</p>
+          </div>
         </div>
       </div>
     </div>
