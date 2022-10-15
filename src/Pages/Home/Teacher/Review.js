b54f7b529d0b9f0aae2b54ec781da5271a0eece9
capmahin <mahinulhuque1996@gmail.com>
2022-10-16 04:44:31 +0600

    add comment section

--- src/Pages/Home/Teacher/Review.js
+++ src/Pages/Home/Teacher/Review.js
@@ -6,9 +6,13 @@ const Review = () => {
       <input type="checkbox" id="review-modal" className="modal-toggle" />
       <div className="modal modal-bottom sm:modal-middle">
         <div className="modal-box">
-          <h3 className="font-bold text-lg">
-            Congratulations random Internet user!
-          </h3>
+          <label
+            for="review-modal"
+            className="btn btn-sm tbn-circle absolute right-2 top-2"
+          >
+            X
+          </label>
+          <h3 className="font-bold text-lg">Comments(8)</h3>
           <p className="py-4">
             You've been selected for a chance to get one year of subscription to
             use Wikipedia for free!
