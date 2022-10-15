c259adc46c0d84f0e4178e31a3eeffdf44a52ea7
capmahin <mahinulhuque1996@gmail.com>
2022-10-16 02:18:17 +0600

    add card component and put in teacher

--- src/Pages/Home/Teacher/Teacher.js
+++ src/Pages/Home/Teacher/Teacher.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Teachers from "./Teachers";
 
 const Teacher = () => {
   const [teacher, setTeacher] = useState([]);
@@ -14,6 +15,11 @@ const Teacher = () => {
         <h3 className="text-black text-3xl font-bold uppercase  py-4">
           Our Teacher List
         </h3>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 justify-items-center gap-3 pt-6 px-5 py-5">
+        {teacher.map((teachers) => (
+          <Teachers key={teacher._id} teachers={teachers}></Teachers>
+        ))}
       </div>
     </div>
   );
---
+++ src/Pages/Home/Teacher/Teachers.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Teachers = ({ teachers }) => {
+  const { name, img, post, exprieance, fees } = teachers;
+  return (
+    <div className="card card-side  bg-base-100 shadow-xl">
+      <figure>
+        <div className="avatar">
+          <div className="w-24 h-20 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+            <img src="https://placeimg.com/192/192/people" />
+          </div>
+        </div>
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">
+          Shoes!
+          <div className="badge badge-secondary">NEW</div>
+        </h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div className="card-actions justify-end align-end">
+          <div className="badge badge-outline">Fashion</div>
+          <div className="badge badge-outline">Products</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Teachers;
