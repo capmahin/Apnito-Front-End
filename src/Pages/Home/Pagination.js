877a80a318d07e75fcffc198a2a85a05cb452136
capmahin <mahinulhuque1996@gmail.com>
2022-10-17 04:13:04 +0600

    button fised

--- src/Pages/Home/Pagination.js
+++ src/Pages/Home/Pagination.js
@@ -4,14 +4,20 @@ const Pagination = () => {
   return (
     <div className="grid justify-items-center">
       <div className="btn-group  ">
+        <div className="  pr-5">
+          <button className="btn btn-outline">«</button>
+        </div>
         <button className="btn btn-outline">1</button>
         <button className="btn btn-outline">2</button>
-        <button className="btn btn-circle btn-secondary ">3</button>
+        <button className="btn btn-outline  ">3</button>
         <button className="btn btn-outline">4</button>
         <button className="btn btn-outline">5</button>
         <button className="btn btn-outline">6</button>
         <button className="btn btn-outline">7</button>
         <button className="btn btn-outline">8</button>
+        <div className="  pl-5">
+          <button className="btn btn-outline ">»</button>
+        </div>
       </div>
     </div>
   );
