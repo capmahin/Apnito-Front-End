76288a96ca6eadb8e6ce65872c177e6eb5d57dec
capmahin <mahinulhuque1996@gmail.com>
2022-10-17 03:56:48 +0600

    finish

--- src/Pages/Home/Teacher/Teachers.js
+++ src/Pages/Home/Teacher/Teachers.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactPlayer from "react-player";
 
 const Teachers = ({ teachers }) => {
-  const { name, img, post, experience, fees, video } = teachers;
+  const { name, img, post, experience, fees, video, address } = teachers;
   return (
     <div className="">
       <div className=" pl-60 w-fit hero shadow ">
@@ -200,6 +200,90 @@ const Teachers = ({ teachers }) => {
                       <div className="hero  ">
                         <div className="hero-content flex-col lg:flex-row-reverse">
                           <div>
+                            <div className="card w-96 bg-base-100 shadow-xl">
+                              <figure>
+                                <div className="stats bg-primary text-primary-content w-96">
+                                  <div className="stat">
+                                    <div className="stat-title pb-8">
+                                      <p className="text-center text-xl text-white">
+                                        $ Fees :
+                                      </p>
+                                    </div>
+                                  </div>
+
+                                  <div className="stat">
+                                    <div className="stat-title  text-white">
+                                      Monthly
+                                    </div>
+                                    <div className="stat-title text-2xl  text-white">
+                                      $89,400
+                                    </div>
+                                  </div>
+                                  <div className="stat">
+                                    <div className="stat-title  text-white">
+                                      Monthly
+                                    </div>
+                                    <div className="stat-title text-2xl  text-white">
+                                      $89,400
+                                    </div>
+                                  </div>
+                                </div>
+                              </figure>
+                              <div className="">
+                                <h2 className="text-2xl pl-5  text-purple-500">
+                                  Booking a time Slut
+                                </h2>
+                                <p className=" pl-5 py-2 font-semibold">
+                                  Morning
+                                </p>
+                                <div className="card-actions justify-start pt-4 pl-8">
+                                  <button className="btn btn-outline btn-primary">
+                                    10.00 AM
+                                  </button>
+                                  <button className="btn btn-outline btn-primary">
+                                    10.30 AM
+                                  </button>
+                                  <button className="btn btn-outline btn-primary">
+                                    11.00 AM
+                                  </button>
+                                </div>
+                                <p className=" pl-5 py-2 font-semibold">
+                                  Afternoon
+                                </p>
+                                <div className="card-actions justify-start pt-4 pl-8">
+                                  <button className="btn btn-outline btn-primary">
+                                    12.00 PM
+                                  </button>
+                                  <button className="btn btn-outline btn-primary">
+                                    1.30 PM
+                                  </button>
+                                  <button className="btn btn-outline btn-primary">
+                                    2.00 PM
+                                  </button>
+                                </div>
+                                <p className=" pl-5 py-2 font-semibold">
+                                  Evening
+                                </p>
+                                <div className="card-actions justify-start pt-4 pl-8">
+                                  <button className="btn btn-outline btn-primary">
+                                    5.00 PM
+                                  </button>
+                                  <button className="btn btn-outline btn-primary">
+                                    6.00 PM
+                                  </button>
+                                  <button className="btn btn-outline btn-primary">
+                                    6.30 PM
+                                  </button>
+                                </div>
+                                <div className="card-actions justify-center pt-4 pb-4 ">
+                                  <button className="btn btn-wide btn-primary">
+                                    Book Now
+                                  </button>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                          <div>
                             <h1 className="text-2xl font-bold">
                               {name}
                               <span className="text-sm pl-2 text-purple-500 font-semibold">
@@ -271,15 +355,35 @@ const Teachers = ({ teachers }) => {
                                 </div>
                               </span>
                             </h1>
-                            <p className="py-6">
-                              Provident cupiditate voluptatem et in. Quaerat
-                              fugiat ut assumenda excepturi exercitationem
-                              quasi. In deleniti eaque aut repudiandae et a id
-                              nisi.
+                            <p className="py-4">
+                              <span className="font-bold">Address</span>:{" "}
+                              {address}
                             </p>
-                            <button className="btn btn-primary">
-                              Get Started
-                            </button>
+                            <p className="pb-2">
+                              <span className="font-bold pr-2">About:</span>Sonu
+                              Nigam is an Indian singer, music director and
+                              actor. He has been described in the media as one
+                              of the most popular and successful playback
+                              singers of Hindi Cinema and Kannada Cinema.
+                            </p>
+                            <p className="pb-2">
+                              Spacial Quality in music:
+                              <span className="font-bold px-2">Vocal</span>
+                            </p>
+                            <p className="pb-2">
+                              No of Student taught:
+                              <span className="font-bold px-2">2500</span>
+                            </p>
+                            <p className="pb-2">
+                              Spacial achiements (if any):
+                              <span className="font-bold px-2">Guitar</span>
+                            </p>
+                            <p className="pb-2">
+                              Chose type of class:
+                              <span className="font-bold px-2">
+                                (Physical/Online)
+                              </span>
+                            </p>
                           </div>
                         </div>
                       </div>
