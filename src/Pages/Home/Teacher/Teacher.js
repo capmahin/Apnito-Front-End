5b7b5e7cb310ec43bc7efeba4d106460559d1755
capmahin <mahinulhuque1996@gmail.com>
2022-10-18 03:54:17 +0600

    almost finish

--- src/Pages/Home/Teacher/Teacher.js
+++ src/Pages/Home/Teacher/Teacher.js
@@ -16,11 +16,11 @@ const Teacher = () => {
     <div>
       <div className="flex justify-between py-6">
         <div className="">
-          <h3 className="text-black text-3xl font-bold uppercase text-start pl-52 pr-8">
+          <h3 className="text-purple-500 text-3xl font-bold uppercase text-start pl-52 pr-8">
             Music Teacher
           </h3>
         </div>
-        <div className="card-actions justify-start pr-8 pl-8">
+        <div className="card-actions justify-start pr-12 pl-8">
           <button className="btn rounded-full btn-primary text-white">
             vocal
           </button>
@@ -28,6 +28,7 @@ const Teacher = () => {
           <button className="btn rounded-full  ">Instrumental</button>
         </div>
       </div>
+
       <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 justify-items-center gap-3 pt-6 px-5 py-5 w-fit">
         {teacher.map((teachers) => (
           <Teachers key={teacher._id} teachers={teachers}></Teachers>
