b7f5f5956e6e5d8e7721a271a70e41d2090cd03a
capmahin <mahinulhuque1996@gmail.com>
2022-10-18 03:32:53 +0600

    add button

--- src/Pages/Home/Teacher/Teacher.js
+++ src/Pages/Home/Teacher/Teacher.js
@@ -14,10 +14,17 @@ const Teacher = () => {
 
   return (
     <div>
-      <div className="text-center">
-        <h3 className="text-black text-3xl font-bold uppercase  py-4">
-          Our Teacher List
-        </h3>
+      <div className="flex justify-between py-6">
+        <div className="">
+          <h3 className="text-black text-3xl font-bold uppercase text-start pl-52 pr-8">
+            Music Teacher
+          </h3>
+        </div>
+        <div className="justify-end items-end px-10">
+          <button className="btn btn-info ">vocal</button>
+
+          <button className="btn btn-info  ">Instrumental</button>
+        </div>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 justify-items-center gap-3 pt-6 px-5 py-5 w-fit">
         {teacher.map((teachers) => (
