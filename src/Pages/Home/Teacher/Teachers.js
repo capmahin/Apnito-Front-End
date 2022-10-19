f1873d2afa82b791a52e805a81d5806516bf8a8b
capmahin <mahinulhuque1996@gmail.com>
2022-10-20 02:37:36 +0600

    solve flex problem

--- src/Pages/Home/Teacher/Teachers.js
+++ src/Pages/Home/Teacher/Teachers.js
@@ -14,91 +14,95 @@ const Teachers = ({ teachers }) => {
           {/*  */}
           {!state.showMore && (
             <>
-              <div className="avatar pb-12">
-                <div className="w-36 h-36 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                  <img src={img} />
+              <div className="flex w-full pr-28">
+                <div className="avatar  pb-12">
+                  <div className="w-36 h-36 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                    <img src={img} />
+                  </div>
                 </div>
-              </div>
-              <div className="pb-12">
-                <div className="flex">
-                  <h1 className="text-xl font-bold">
-                    {name}{" "}
-                    <span className="text-sm text-primary font-semibold">
-                      {post}
-                    </span>
-                  </h1>
+                <div className="pb-12">
+                  <div className="flex">
+                    <h1 className="text-xl font-bold">
+                      {name}{" "}
+                      <span className="text-sm text-primary font-semibold">
+                        {post}
+                      </span>
+                    </h1>
+                  </div>
+                  <p className="text-sm font-semibold">
+                    {experience} experience
+                  </p>
+                  <p className=" text-sm text-primary font-semibold">
+                    Hourly fees ${fees}
+                  </p>
+                  <div className="rating rating-sm rating-half pr-6">
+                    <input
+                      type="radio"
+                      name="rating-10"
+                      className="rating-hidden"
+                    />
+                    <input
+                      type="radio"
+                      name="rating-10"
+                      className="bg-primary mask mask-star-2 mask-half-1"
+                    />
+                    <input
+                      type="radio"
+                      name="rating-10"
+                      className="bg-primary mask mask-star-2 mask-half-2"
+                    />
+                    <input
+                      type="radio"
+                      name="rating-10"
+                      className="bg-primary mask mask-star-2 mask-half-1"
+                      checked
+                    />
+                    <input
+                      type="radio"
+                      name="rating-10"
+                      className="bg-primary mask mask-star-2 mask-half-2"
+                    />
+                    <input
+                      type="radio"
+                      name="rating-10"
+                      className="bg-primary mask mask-star-2 mask-half-1"
+                    />
+                    <input
+                      type="radio"
+                      name="rating-10"
+                      className="bg-primary mask mask-star-2 mask-half-2"
+                    />
+                    <input
+                      type="radio"
+                      name="rating-10"
+                      className="bg-primary mask mask-star-2 mask-half-1"
+                    />
+                    <input
+                      type="radio"
+                      name="rating-10"
+                      className="bg-primary mask mask-star-2 mask-half-2"
+                    />
+                    <input
+                      type="radio"
+                      name="rating-10"
+                      className="bg-primary mask mask-star-2 mask-half-1"
+                    />
+                    <input
+                      type="radio"
+                      name="rating-10"
+                      className="bg-primary mask mask-star-2 mask-half-2"
+                    />
+                    <p className="pl-2 text-sm font-bold">4.5</p>
+                  </div>
+                  <button className="flex">
+                    <label
+                      htmlFor="review-modal"
+                      className=" btn btn-secondary text-black rounded-full btn-sm"
+                    >
+                      Reviews
+                    </label>
+                  </button>
                 </div>
-                <p className="text-sm font-semibold">{experience} experience</p>
-                <p className=" text-sm text-primary font-semibold">
-                  Hourly fees ${fees}
-                </p>
-                <div className="rating rating-sm rating-half pr-6">
-                  <input
-                    type="radio"
-                    name="rating-10"
-                    className="rating-hidden"
-                  />
-                  <input
-                    type="radio"
-                    name="rating-10"
-                    className="bg-primary mask mask-star-2 mask-half-1"
-                  />
-                  <input
-                    type="radio"
-                    name="rating-10"
-                    className="bg-primary mask mask-star-2 mask-half-2"
-                  />
-                  <input
-                    type="radio"
-                    name="rating-10"
-                    className="bg-primary mask mask-star-2 mask-half-1"
-                    checked
-                  />
-                  <input
-                    type="radio"
-                    name="rating-10"
-                    className="bg-primary mask mask-star-2 mask-half-2"
-                  />
-                  <input
-                    type="radio"
-                    name="rating-10"
-                    className="bg-primary mask mask-star-2 mask-half-1"
-                  />
-                  <input
-                    type="radio"
-                    name="rating-10"
-                    className="bg-primary mask mask-star-2 mask-half-2"
-                  />
-                  <input
-                    type="radio"
-                    name="rating-10"
-                    className="bg-primary mask mask-star-2 mask-half-1"
-                  />
-                  <input
-                    type="radio"
-                    name="rating-10"
-                    className="bg-primary mask mask-star-2 mask-half-2"
-                  />
-                  <input
-                    type="radio"
-                    name="rating-10"
-                    className="bg-primary mask mask-star-2 mask-half-1"
-                  />
-                  <input
-                    type="radio"
-                    name="rating-10"
-                    className="bg-primary mask mask-star-2 mask-half-2"
-                  />
-                  <p className="pl-2 text-sm font-bold">4.5</p>
-                </div>
-                <button className="flex">
-                  <label
-                    htmlFor="review-modal"
-                    className=" btn btn-secondary text-black rounded-full btn-sm"
-                  >
-                    Reviews
-                  </label>
-                </button>
               </div>
             </>
           )}
